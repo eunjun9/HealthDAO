7b3ac53d738dbb1b0a7fea1d625c20bf27e7b438
eunjun9 <wud29@naver.com>
2021-12-28 20:40:10 +0900

    first commit

---
+++ src/main/java/com/example/demo/configuration/HealthDaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDaoApplication.class, args);
+	}
+
+}
