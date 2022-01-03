3440eca12bf2722c642ab8a1e56ef900fb23242a
유가희 <ygh9040@naver.com>
2022-01-04 00:36:18 +0900

    리스트 보이지만 완성은 아닌

--- src/main/java/com/kh/healthDao/mypage/model/service/MypageServiceImpl.java
+++ src/main/java/com/kh/healthDao/mypage/model/service/MypageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kh.healthDao.mypage.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,11 @@ public class MypageServiceImpl implements QnaService{
 	@Override
 	public int qnaModify(Qna modifyQna) {
 		return 0;
+	}
+
+	@Override
+	public List<Qna> findQnaList() {		
+		return mypageMapper.findQnaList();
 	}
 
 }
--- src/main/java/com/kh/healthDao/mypage/model/service/QnaService.java
+++ src/main/java/com/kh/healthDao/mypage/model/service/QnaService.java
@@ -1,11 +1,15 @@
 package com.kh.healthDao.mypage.model.service;
 
+import java.util.List;
+
 import com.kh.healthDao.mypage.model.vo.Qna;
 
 public interface QnaService {
 	// 문의하기
 	int qnaInsert(Qna newQna);
 	// (사용자) 문의 수정하기
-	int qnaModify(Qna modifyQna);
+	int qnaModify(Qna modifyQna);	
+	// (사용자) 문의 리스트
+	List<Qna> findQnaList();
 	
 }
