93071d4dbb9213b521d958d92350d37d4d0c1cb9
ryu <roksellid01@gmail.com>
2022-01-04 16:55:52 +0900

    .

--- src/main/java/com/kh/healthDao/main/model/controller/MainController.java
+++ src/main/java/com/kh/healthDao/main/model/controller/MainController.java
@@ -1,13 +1,11 @@
 package com.kh.healthDao.main.model.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.healthDao.main.model.service.BannerService;
