308c2c9ed993e95152afc0ec6ffe754257708208
윤용한 <yoonyh95@naver.com>
2022-01-05 19:15:59 +0900

    test

--- src/main/java/com/kh/healthDao/member/model/vo/Member.java
+++ src/main/java/com/kh/healthDao/member/model/vo/Member.java
@@ -3,11 +3,15 @@ package com.kh.healthDao.member.model.vo;
 import java.util.Date;
 import java.util.List;
 
+import com.kh.healthDao.trainer.model.vo.Trainer;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 	
 	private int userNo;				// 유저번호
