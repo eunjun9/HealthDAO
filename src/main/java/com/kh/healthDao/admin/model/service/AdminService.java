898a0f58cd86bcec3819d5377a5ee32a43f9c199
Changseop123 <fhgp0722@hanmail.net>
2022-01-06 00:24:54 +0900

    repush_changseop_food, beverage, goods page list

--- src/main/java/com/kh/healthDao/admin/model/service/AdminService.java
+++ src/main/java/com/kh/healthDao/admin/model/service/AdminService.java
@@ -1,18 +1,12 @@
 package com.kh.healthDao.admin.model.service;
 
-import java.util.List;
-
 import com.kh.healthDao.admin.model.vo.Product;
+
 
 public interface AdminService {
 	
 	
-	public void registNewProduct(Product newProduct);
-	
 	// 상품 등록
 	public int RegistProduct(Product product);
-	
-	// 재고내역
-	List<Product> listProductInventory();
 
 }
--- src/main/java/com/kh/healthDao/shopping/model/dao/ShoppingMapper.java
+++ src/main/java/com/kh/healthDao/shopping/model/dao/ShoppingMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.kh.healthDao.admin.model.vo.Product;
 import com.kh.healthDao.shopping.model.vo.Shopping;
 
 
@@ -11,9 +12,13 @@ import com.kh.healthDao.shopping.model.vo.Shopping;
 @Mapper
 public interface ShoppingMapper {
 
-	List<Shopping> ShoppingList();
+	List<Product> ShoppingList();
 
-	Shopping ShoppingSelect(int tNo);
+	Product NewProductSelect(int productNo);
 
+	List<Product> foodShoppingList();
 	
+	List<Product> beverageShoppingList();
+
+	List<Product> goodsShoppingList();
 }
