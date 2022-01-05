33c799df11e5bfd427eb74b11018ad4f0d0f372a
645 <645@DESKTOP-JDLJDEN>
2022-01-05 21:38:03 +0900

    파일업로드 등록

--- src/main/java/com/kh/healthDao/main/model/dao/BannerMapper.java
+++ src/main/java/com/kh/healthDao/main/model/dao/BannerMapper.java
@@ -1,6 +1,7 @@
 package com.kh.healthDao.main.model.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,8 +11,12 @@ import com.kh.healthDao.main.model.vo.PageInfo;
 
 @Mapper
 public interface BannerMapper {
-//	List<Banner> bannerAllList(PageInfo pi);
-//	int getListCount();
 
 	List<Banner> bannerAllList();
+
+	int insertBanner(Map<String, Object> map);
+
+	int insertFile(Map<String, Object> map);
+
+	int insertFileDB();
 }
