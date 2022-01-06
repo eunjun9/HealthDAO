193aa0f5964b0c3ad8dea6e239798ae745e8065b
Changseop123 <fhgp0722@hanmail.net>
2022-01-07 01:14:37 +0900

    product page paging

--- src/main/java/com/kh/healthDao/shopping/model/service/ShoppingService.java
+++ src/main/java/com/kh/healthDao/shopping/model/service/ShoppingService.java
@@ -1,22 +1,34 @@
 package com.kh.healthDao.shopping.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.kh.healthDao.admin.model.vo.Product;
 
 
 
 public interface ShoppingService {
-	List<Product> ShoppingList();
+	
+	Map<String, Object> ShoppingList(int page);
+
+	Map<String, Object> foodShoppingList(int page);
+
+	Map<String, Object> beverageShoppingList(int page);
+
+	Map<String, Object> goodsShoppingList(int page);
 
 	Product NewProductSelect(int productNo);
-
-	List<Product> foodShoppingList();
 
 	List<Product> beverageShoppingList();
 
 	List<Product> goodsShoppingList();
 
-	Product shoppingSelect(int productNo);
+	Product shoppingDetail(int productNo);
+
+
+
+
+
+	
 
 }
