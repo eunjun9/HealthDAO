fbf9ecb7bd3d2586ecdf644af4ec3c9e2ea22160
645 <645@DESKTOP-JDLJDEN>
2022-01-14 17:01:12 +0900

    .

--- src/main/resources/static/js/ui.js
+++ src/main/resources/static/js/ui.js
@@ -1154,3 +1154,27 @@ function addCookie(id) {
 		setCookie('productItems', id, expire);
 	}
 }
+
+/**
+ * 장바구니 추가 ajax
+ */
+function cartInsert(pNo){
+	let productNo = pNo;
+	
+	var sendData = {"productNo":productNo, "cartStock":1};
+	$.ajax({
+		url : "/mypage/cartInsert",
+		data : sendData,
+		type : "post",
+		success : function(data){
+			if(data == "success"){
+				alert("장바구니에 추가되었습니다.");							
+			}else{
+				alert("장바구니에 추가 실패");
+			}
+		},
+		error : function(e){
+			console.log(e)
+		}
+	});
+}
